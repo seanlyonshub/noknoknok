47e2b8e86a8522625e044463283bef304709e455
Ji <87422102+inthenameofJi@users.noreply.github.com>
2022-02-10 23:10:53 +0100

    Add files via upload

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -6,7 +6,6 @@ export (int) var dialogue_index
 export (bool) var awake = false
 export (String) var npc_type = "enemy"
 export (bool) var chases_npcs = true
-export (int) var chase_distance = 32
 
 export (NodePath) var target_path = null
 export (NodePath) var gun_path = null
@@ -16,18 +15,21 @@ var direction : Vector2
 var target_position : Vector2
 
 var dead := false
+var disintegrated := false
 
 var target
+var target_index : int
 var gun
 var home
+var ghost_animations
 
-var target_index : int
 var timer : int
 
 onready var audio_player = self.owner.get_node("audio")
 onready var upgrades = get_parent().get_parent().get_node("upgrades")
 onready var animations = get_node("animations")
 onready var ray = get_node("ray")
+onready var chase_distance = speed * 3
 
 func _ready() -> void:
 	if !awake:
@@ -37,6 +39,8 @@ func _ready() -> void:
 		target = get_node(target_path)
 	if gun_path != null:
 		gun = get_node(gun_path)
+	if npc_type == "ghost":
+		ghost_animations = get_node("ghost_animations")
 
 func awaken() -> void:
 	if !is_physics_processing():
@@ -99,7 +103,7 @@ func _physics_process(_delta: float) -> void:
 					if npc.is_physics_processing():
 						if npc.npc_type == "priest":
 							if position.distance_to(npc.position) < 8:
-								kill()
+								disintegrate()
 
 func wander() -> void:
 		if timer % 60 == 0:
@@ -115,11 +119,12 @@ func chase_target() -> void:
 	if is_instance_valid(target):
 		direction = ray.cast_to.normalized()
 
-		if position.distance_to(target_position) > chase_distance and chase_distance != 0:
+		if position.distance_to(target_position) > chase_distance:
 			if npc_type == "dog":
+				print(name, "awa: ", target)
 				get_next_target()
 			else:
-				velocity = Vector2.ZERO
+				direction = Vector2.ZERO
 
 		if is_instance_valid(gun):
 			if gun.ammo > 0:
@@ -132,27 +137,31 @@ func chase_target() -> void:
 				gun.queue_free()
 
 	else:
-		if chases_npcs and npc_type == "dog":
+		if npc_type == "dog":
 			get_next_target()
 		else:
 			target = home
 
 func get_next_target() -> void:
 	var npcs = get_parent().get_children()
-	npcs.erase(self)
-
-	target_index = wrapi(target_index, 0, npcs.size())
-
 	var next_target = npcs[target_index]
 
-	if next_target.is_physics_processing() and position.distance_to(next_target.position) < chase_distance:
-		target = next_target
-	else:
-		target_index += 1
+	target_index += 1
+	target_index %= npcs.size()
 
-func is_node_npc(node) -> bool:
-	if "npc_type" in node:
-		return true
+	if is_instance_valid(next_target):
+		if next_target.is_physics_processing():
+			if next_target.position.distance_to(target_position) < chase_distance or !is_node_npc(next_target, "dog"):
+				target = next_target
+
+func is_node_npc(node, npc: String = "") -> bool:
+	if npc == "":
+		if "npc_type" in node:
+			return true
+	else:
+		if "npc_type" in node:
+			if node.npc_type == npc:
+				return true
 	return false
 
 func get_collider():
@@ -167,3 +176,11 @@ func is_ray_casting_at_wall() -> bool:
 		if ray.get_collider().name.rstrip("0123456789") == "wall":
 			return true
 	return false
+
+func disintegrate() -> void:
+	if !disintegrated:
+		audio_player.stream = audio_player.dialogue[19]
+		audio_player.play()
+		ghost_animations.play("disintegrate")
+
+		disintegrated = true
